7af44494828a52beb5c9dad2d10e65e09e040877
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-16 16:22:08 +0900

    change configuration

--- knexfile.js
+++ knexfile.js
@@ -4,37 +4,10 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      database: 'codemirror',
-    },
+    connection: 'postgres://localhost/codemirror',
   },
-
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'codemirror',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
   },
-
 };
