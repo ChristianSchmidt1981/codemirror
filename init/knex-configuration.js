386963801f7e017031ae32f7fe6a0bb9c9262a0f
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-16 16:04:19 +0900

    playing

--- init/knex-configuration.js
+++ init/knex-configuration.js
@@ -1,5 +1,6 @@
 const configurationFromKnex = require('../knexfile');
 
+console.log('environment9999', process.env.DATABASE_URL, process.env.NODE_ENV);
 const environment = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 
 const knex = require('knex')(configurationFromKnex[environment]);
