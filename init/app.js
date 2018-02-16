543a5eb21739814feb74fc2ea69dbe1254a37564
Christian Schmidt <cschmidt.solingen@gmail.com>
2018-02-16 16:33:27 +0900

    add public again

--- init/app.js
+++ init/app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'));
 
 
 app.use('/api', index);
-// app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // catch 404 and forward to error handler
 app.use((request, response, next) => {
